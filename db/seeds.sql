c4e2707b5b09ca413780e3246e0a0c26c542eb51
mannygarcia98 <mannysg88@gmail.com>
2022-04-28 13:04:23 -0500

    add role table

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,13 +1,13 @@
--- INSERT INTO role (title, salary)
--- VALUES 
---   ('Sales Manager', 120000),
---   ('Salesperson', 80000),
---   ('Lead Engineer', 150000),
---   ('Software Engineer', 120000),
---   ('Account Manager', 160000),
---   ('Accountant', 125000),
---   ('Legal Team Lead', 250000),
---   ('Lawyer', 190000);
+INSERT INTO role (title, salary)
+VALUES 
+  ('Sales Manager', 120000),
+  ('Salesperson', 80000),
+  ('Lead Engineer', 150000),
+  ('Software Engineer', 120000),
+  ('Account Manager', 160000),
+  ('Accountant', 125000),
+  ('Legal Team Lead', 250000),
+  ('Lawyer', 190000);
 
 INSERT INTO employee (first_name, last_name)
 VALUES 
