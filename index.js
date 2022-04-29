8eff75e13b2e18b7ac662f5c567d9823122c8648
mannygarcia98 <mannysg88@gmail.com>
2022-04-29 12:41:14 -0500

    add inquirer prompts for viewing all departments, roles, or employees

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
 const db = require("./db/connection");
 const cTable = require("console.table");
-// db.connect();
+const inquirer = require("inquirer");
 
 db.connect((err) => {
   if (err) throw err;
-  console.log("Database connected.");
+  // console.log("Database connected.");
 });
+
+// const promptUser = () => {
+//   return inquirer.prompt([
+//     {
+//       type: "list",
+//       name: "action",
+//       message: "What would you like to do?",
+//       choices: ["View all departments", "View all roles", "View all employees"],
+//     },
+//   ]);
+// };
+
+const prompts = () => {
+  return inquirer.prompt([
+    {
+      type: "list",
+      name: "action",
+      message: "What would you like to do?",
+      choices: ["View all departments", "View all roles", "View all employees"],
+    },
+  ]);
+};
 
 // view all departments
 const viewAllDepartments = () => {
@@ -14,6 +36,7 @@ const viewAllDepartments = () => {
     All departments
     `);
     console.table(rows);
+    promptUser();
   });
 };
 
@@ -31,13 +54,14 @@ const viewAllRoles = () => {
       All roles
       `);
       console.table(rows);
+      promptUser();
     }
   );
 };
 
 const viewAllEmployees = () => {
   db.query(
-    `SELECT employee.id, employee.first_name, employee.last_name, role.title AS 'role', department.name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager 
+    `SELECT employee.id, employee.first_name, employee.last_name, role.title AS role, department.name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager 
     FROM employee LEFT JOIN role on employee.role_id = role.id 
     LEFT JOIN department department on role.department_id = department.id 
     LEFT JOIN employee manager on manager.id = employee.manager_id;`,
@@ -49,6 +73,41 @@ const viewAllEmployees = () => {
     All employees
     `);
       console.table(rows);
+      promptUser();
     }
   );
 };
+
+// promptUser()
+//   .then((action) => {
+//     const { choice } = action;
+//     if (choice = "View all departments")
+//   })
+
+// promptUser().then((response) => {
+//   const { action } = response;
+//   if (action === "View all departments") {
+//     viewAllDepartments();
+//   } else if (action === "View all roles") {
+//     viewAllRoles();
+//   } else {
+//     viewAllEmployees();
+//   }
+//   promptUser();
+//   // console.log(action);
+// });
+
+const promptUser = () => {
+  prompts().then((response) => {
+    const { action } = response;
+    if (action === "View all departments") {
+      viewAllDepartments();
+    } else if (action === "View all roles") {
+      viewAllRoles();
+    } else {
+      viewAllEmployees();
+    }
+  });
+};
+
+promptUser();
